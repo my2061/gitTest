a486dc67fd8733cfca666908499b0a79ebfd9e0a
mengyang <2402334261@qq.com>
2022-10-28 10:06:52 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: mengyang 2402334261@qq.com
+ * @Date: 2022-10-28 10:03:38
+ * @LastEditors: mengyang 2402334261@qq.com
+ * @LastEditTime: 2022-10-28 10:03:47
+ * @FilePath: /xbb-learn/gitTest/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+function test1(){
+    console.log("12390810938");
+}
